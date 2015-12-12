e8d1f5318f38db0db71b0446d61271005240217c
apallier <a.pallier@laposte.net>
2015-12-12 23:14:49 +0100

    Ajout de la gestion d'une config privée (non commitée)

--- config.py
+++ config.py
@@ -2,15 +2,22 @@
 # -*- coding: utf-8 -*-
 """ Configuration du programme d'alerte pour le tarif EJP """
 
-ZONE = 'EjpSud'  # Valeur possible : "EjpOuest" "EjpPaca" "EjpNord" "EjpSud"
+try:
+    # Si une config particulière existe (évite de commiter sa config)
+    import my_config, __builtin__
+    __builtin__.config = my_config
 
-FROM_ADDR = '<mon-adresse>@gmail.com'
-PASSWORD = '<mon_password_mail>'
+except ImportError:
 
-TO_ADDRS = ['<adresse_1>@hotmail.com', '<adresse_2>@laposte.net']
+    ZONE = 'EjpSud'  # Valeur possible : "EjpOuest" "EjpPaca" "EjpNord" "EjpSud"
 
-# Exemples d'adresses de l'API de Freemobile (à adapter)
-TO_FREEMOBILE_API = [
-    'https://smsapi.free-mobile.fr/sendmsg?user=12345678&pass=4HcpERTXEd2Igj',
-    'https://smsapi.free-mobile.fr/sendmsg?user=87654321&pass=aQsTR5XtQfTnAq'
-]
+    FROM_ADDR = '<mon-adresse>@gmail.com'
+    PASSWORD = '<mon_password_mail>'
+
+    TO_ADDRS = ['<adresse_1>@hotmail.com', '<adresse_2>@laposte.net']
+
+    # Exemples d'adresses de l'API de Freemobile (à adapter)
+    TO_FREEMOBILE_API = [
+        'https://smsapi.free-mobile.fr/sendmsg?user=12345678&pass=4HcpERTXEd2Igj',
+        'https://smsapi.free-mobile.fr/sendmsg?user=87654321&pass=aQsTR5XtQfTnAq'
+    ]
